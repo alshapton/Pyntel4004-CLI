6c0d4c58559d08e75a129dcc8176c4e5bb7306eb
alshapton <alshapton@gmail.com>
2022-02-24 01:54:34 +0000

    Added logos

--- setup.py
+++ setup.py
@@ -5,8 +5,9 @@ setup(
     version='0.0.1-alpha',
     py_modules='4004cli',
     install_requires=[
-        'Click',
-	],
+        'Click'
+        'Pyntel4004',
+    ],
     entry_points='''
         [console_scripts]
         4004=4004cli:cli
